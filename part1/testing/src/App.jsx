52aca2f56bbba40e88b7c6b01fb0e3db03eb9a1e
Exi <eximo888@gmail.com>
2023-10-24 04:01:20 +0200

    excersise 1.1 and 1.2

---
+++ part1/testing/src/App.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable react/prop-types */
+const Hello = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  );
+};
+
+const App = () => {
+  const name = 'Peter';
+  const age = 10;
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+    </div>
+  );
+};
+
+export default App;
