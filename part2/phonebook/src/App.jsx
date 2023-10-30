9e2b2a03c0e6b9516a7320719c0138a3ff4a2de2
Exi <eximo888@gmail.com>
2023-10-30 12:16:33 +0100

    The Phonebook Step1

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,40 @@
+/* eslint-disable react/prop-types */
+import { useState } from 'react';
+const Entry = ({ person }) => {
+  return (
+    <>
+      <p key={person.name}>{person.name}</p>
+    </>
+  );
+};
+const App = () => {
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [newName, setNewName] = useState('');
+  const addPerson = (e) => {
+    e.preventDefault();
+    setPersons(persons.concat({ name: newName }));
+    setNewName('');
+  };
+  const handleOnChange = (e) => {
+    setNewName(e.target.value);
+  };
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleOnChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((value) => (
+        <Entry key={value.name} person={value} />
+      ))}
+    </div>
+  );
+};
+
+export default App;
